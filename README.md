9372baf9068e4baedcc1928a91cb33ede490d85f
Rahat Khan Tusar(RKT) <69615463+r3k4t@users.noreply.github.com>
2020-12-26 22:46:49 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,27 @@
+<h2>RKTAPF<h2>
+
+<h6>Author : RKT</h6>
+
+
+![Screenshot_2020-12-26_22-30-18](https://user-images.githubusercontent.com/69615463/103156128-8b8ae180-47cb-11eb-8991-106cc57081be.png)
+
+
+RKTAPF is a Admin Page Finder tool.Its build on python program.
+
+
+### Setup ###
+
+
++ sudo pip install -r requirements.txt
+
+
+### Terminal Command ###
+
++ git clone https://github.com/r3k4t/rktapf.git
++ cd rktapf
++ sudo python3 rktapf.py
+
+### Example ###
+
+[Screenshot_2020-12-26_22-33-02](https://user-images.githubusercontent.com/69615463/103156147-d573c780-47cb-11eb-9d7c-fe435ba432fc.png)
+
---
+++ rktapf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import os
+import sys
+import pyfiglet
+from urllib.request import *
+from urllib.error import *
+
+os.system(chr(27)+"[36m")
+
+os.system("clear")
+
+banner = pyfiglet.figlet_format("RKTAPF",font="standard")
+print(banner)
+
+print("   Author : Rahat Khan Tusar(RKT)")
+print("  Github : https://github.com/r3k4t")
+
+print
+
+#Open links file
+
+file = open('link.txt','r')
+link = input('Enter The Website Link(www.site.com) : ')
+
+for i in range(10000000000000000000000):
+   sub_link = file.readline().split('\n')[0]
+   if not sub_link:
+      break
+   req_link ='http://'+link+'/'+sub_link
+   try:
+      response =urlopen(req_link)
+   except HTTPError as e:
+      continue
+   except URLError as e:
+      continue
+   else:
+      print(" ==[Found]==> ",req_link)
